6c0640964ed1c36191339ca611b30337676f1c2d
Gerardo Flores <douglas.flores@facela.com>
2020-07-09 08:02:38 -0600

    Iniciando NavBar

---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="pa-10">
+        <v-img src="@/assets/logo.svg" width="30" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavBar"
+}
+</script>
+
+<style scoped>
+div {
+    background: var(--primary);
+}
+</style>
